427f4c5b4fdc808c441a657bf5f1cf076039ef0c
Lauren Welborn <laurenwelborn@Laurens-MacBook-Pro.local>
2017-07-18 19:41:45 -0600

    done

--- starter-code/app/js/jquery.app.js
+++ starter-code/app/js/jquery.app.js
@@ -1,11 +1,12 @@
-
+$( document ).ready(function() {
+    console.log( "ready!" );
 
 // ------ get data types on page from server ------ //
 var ajax = $.get('https://ga-cat-rescue.herokuapp.com/api/cats')
 	// data has all cat objects
     .done(function(data) {
         $("#cats").append("<li>",data);
-    });
+});
 
 
 // ------ turns string into objects & line items ------ //
@@ -22,7 +23,33 @@ $.ajax({
 
 
 // ------ Add new cats ------ //
+
 $("form").on("submit", function(event){
+	console.log( "form exists" );
+	// Stop the form from submitting
     event.preventDefault();
+
+    var newCatName = $("#cat-name").val();
+	var newCatNote = $("#cat-note").val();
+
+	var catObject = {
+		name: newCatName,
+		note: newCatNote,
+	}
+
+	// posh pushes onto page, stringify turns objects into string
+	$.post('https://ga-cat-rescue.herokuapp.com/api/cats', JSON.stringify(catObject))
+    .done(function(data){
+    	console.log("calling to data");
+    // parse adds the string as objects
+    var newCatData = JSON.parse(data);
+    $('#cats').append('<li>' + newCatData.name + ' - <em>' + newCatData.note + '</em></li>');
+    });
+
 });
+});
+
+
+
+
 
